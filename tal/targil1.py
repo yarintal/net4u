973338f7a7a4e6300b6d308993011e3a5e7d45f3
yarin <ptmazkirut@gmail.com>
2021-02-10 13:45:01 +0200

    This is my first exercise

--- tal/targil1.py
+++ tal/targil1.py
@@ -1,1 +1,26 @@
-print("hello world")
+a=12
+b=11.56
+c="yarin tal"
+
+#this is integer
+print (a)
+print (type(a))
+
+#this is Float
+print (b)
+print (type(b))
+
+#this is string
+print (c)
+print (type(c))
+
+
+#עבודה
+name=input ("enter you're name please: ")
+age="23"
+mail=input ("Enter your mail: ")
+print(mail)
+print(type(mail))
+
+print("Hello "," \n my Name is: " + name + " \nMy age is: " +str(age))
+
