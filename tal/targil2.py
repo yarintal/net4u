ee63c0b05cca92884586b1a2c1af744d4b5ed492
yarin <ptmazkirut@gmail.com>
2021-02-10 14:18:33 +0200

    Math

---
+++ tal/targil2.py
@@ -0,0 +1,11 @@
+#######math: *   / +    / -  / ** בחזקת /  \lehalek
+num=5
+print("Your number is: " + str(num))
+print (type(num))
+num=num+2
+print("Your number is: " + str(num))
+num2=num*10
+print("Your number is: " + str(num2))
+a=3
+b=3**3
+print(b)
