aeef80f612db1e7cd683a696c501c51c8df58866
yarin <ptmazkirut@gmail.com>
2021-02-11 13:23:22 +0200

    Math

--- tal/targil2.py
+++ tal/targil2.py
@@ -1,18 +1,23 @@
-'''
+
 אלפים=4
 מאות=5
 עשרות=6
 אחדות=7
-'''
+
 num=int(input("Enter a number with 4 digits: "))
+
+#בצורה לא יפה:
 #תרגיל מספר אחד
-print("Alafim= " + str(num//1000))
+#print("Alafim= " + str(num//1000))
 
 #תרגיל מספר שתיים
-print("Meot= " + str(num%1000//100))
+#print("Meot= " + str(num%1000//100))
 
 #תרגיל מספר שלוש
-print("Asarot= " +str((num%100)//10))
+#print("Asarot= " +str((num%100)//10))
 
 #תרגיל מספר ארבע
-print("Ahadot= " + str (num%10))
+#print("Ahadot= " + str (num%10))
+
+#בצורה יפה
+print("Alafim= " + str(num//1000) + "\nMeot= " + str(num%1000//100) + "\nAsarot= " +str((num%100)//10) + "\nAhadot= " + str(num%10))
