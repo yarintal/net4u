1a56b2076132723fa1c69ed7831519fd25d4fb06
yarin <ptmazkirut@gmail.com>
2021-02-11 15:53:31 +0200

    LAB2

---
+++ tal/targil3.py
@@ -0,0 +1,37 @@
+#a="Yarin "
+#b="23"
+#c="blabla@gmail.com"
+#print("Full Name:Yarin\nAge:23\nemail: blabla@gmail.com")
+
+# d=''' - שיטה נוספת
+#Full name: Yarin
+#Age: 23
+#Mail: blabla@gmail.com
+#'''
+#print (d)
+
+#פורמט
+# print("p1={},p2={},p3{},p4{}" .format(1,1.0,"yarin", "BBFD"))
+
+#אמת או שקר
+#print("Yarin" in "Hello Yarin fdkfldkl\nWe recieved your mail...")
+
+#יש שיטה נוספת לעשות אמת או שקר
+#a="yarin"
+#print("yarin" in "Hello yarin fsdjkfjksej")
+
+#להדפיס רק תו מסוים, למשל במקרה הזה רק את האות הראשונה print(s1[0])
+#מתחילים לספור מ0 עד X
+##print(s1[1:-1]) - יביא לי רק את BCDE
+#print(s1[-1]) - סופר הפוך, מהאות האחרונה
+#print(s1[1:]) - מביא הכל מלבד A
+#להדפיס הכל
+#print(s1[:])
+#לעשות קפיצות של 2
+#print(s1[::2])
+#ACE ידפיס לי
+#להדפיס הפוך את כל התו
+#print(s1[::-1]) - אצטרך את זה בהמשך
+
+s1="ABCDEF"
+print (s1[])
