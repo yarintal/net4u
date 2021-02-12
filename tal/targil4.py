b32a1bb0617a6c3225ae1fa66916f03abe964609
yarin <ptmazkirut@gmail.com>
2021-02-12 14:28:24 +0200

    LAB2

---
+++ tal/targil4.py
@@ -0,0 +1,6 @@
+name="yarin t"
+age=23
+mail="yarin@gmail.com"
+print("Full Name: " + name + "\nage:" + str(age) + "\nMail:" + mail)
+print("\n\nFull Name: " + name[::-1] + "\nage:" + str(age*3) )
+print("yarin" in "yarin ben dudu yuval shimon yael gal adam shahar yana")
