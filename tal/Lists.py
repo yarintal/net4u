13279295c2e25e275c22348a3e745a8a0e71056b
yarin <ptmazkirut@gmail.com>
2021-02-13 12:09:55 +0200

    Lists

---
+++ tal/Lists.py
@@ -0,0 +1,23 @@
+#שיטה שעובדים איתה תמיד
+#new_list=[]
+#שיטה שלוקחים סטרינג ומהמירים אותה לליסט
+#new_list2=list()
+
+#new_list=[2,6.6,"yarin blabla",[]]
+#print(type(new_list))
+#new_list2=new_list+[77, "hello world"]
+#print(new_list2)
+#new_list3=new_list2+new_list
+#print_new_list3=new_list*3
+#print(new_list3)
+
+#יביא לי רק את שורה 2
+#new_list=[2,6.6,"yarin blabla",44]
+#print(new_list)
+#print(new_list[2])
+
+my_list=[1,2,23,6.6,"dudu cohen"]
+print("My age: " + str(my_list[2]))
+
+my_list2=my_list*2
+print(my_list2)
