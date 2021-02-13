0bd287dd1f7ee1140aae26ebc9a1f3cb5fc05a11
yarin <ptmazkirut@gmail.com>
2021-02-13 12:40:01 +0200

    Lists

--- tal/Lists.py
+++ tal/Lists.py
@@ -16,8 +16,27 @@
 #print(new_list)
 #print(new_list[2])
 
-my_list=[1,2,23,6.6,"dudu cohen"]
-print("My age: " + str(my_list[2]))
+#my_list=[1,2,23,6.6,"dudu cohen"]
+#print("My age: " + str(my_list[2]))
 
-my_list2=my_list*2
-print(my_list2)
+#לקח את כל הסטרינג הזה והפך אותו לליסט
+#my_list2=list("1234567")
+#print(my_list2)
+
+#עושה הפוך, מליסט לסטרינג עכשיו זה יהיה 1234567 במסך
+#אפשר גם להוסיף תווים בגרשיים למשל 'A' ואז יהיה 1A ו2A ו3A
+#my_string=''.join(my_list2)
+#print(my_string)
+
+#שם את הכל בתא אחד אבל רק אם אין רווחים
+#my_list3=my_string.split()
+#print(my_list3)
+
+#שם לי כל שורה בתא בפני עצמו
+#my_string="hello yarin \nhow are you?"
+#my_list3=my_string.splitlines()
+#print(my_list3)
+
+#מדפיס לי את אורך המשפט - למשל במקרה הזה מדובר כאן ב4
+mylist=["hello", 1, 66.6, "Yarin"]
+print("The length is " + str(len(mylist)))
