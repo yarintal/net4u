034ee9cae2655a41fc7054683689efa8dedef29a
yarin <ptmazkirut@gmail.com>
2021-02-13 13:43:50 +0200

    Lists

--- tal/Lists.py
+++ tal/Lists.py
@@ -38,5 +38,25 @@
 #print(my_list3)
 
 #מדפיס לי את אורך המשפט - למשל במקרה הזה מדובר כאן ב4
-mylist=["hello", 1, 66.6, "Yarin"]
-print("The length is " + str(len(mylist)))
+#mylist=["hello", 1, 66.6, "Yarin"]
+#print("The length is " + str(len(mylist)))
+
+#מראה לי כמה תווים יש במלל שבמרכאות
+#my_str="1232334343535346464647"
+#print(len(my_str))
+
+#הוספת מלל, במקרה הזה מדובר בוואו ויוסי דה קינג ופופ מבטל לי את התו האחרון במקרה הזה זה יוסי דה קינג ומשאיר רק את דודו המלך, 6 זה השורה
+#my_list.append("wow")
+#my_list.append("yossitheking")
+#print(my_list)
+#print("The length is " + str(len(my_list)))
+
+#my_list.insert(6,"dudu the king")
+#print(my_list)
+
+#my_list.pop(6)
+#print(my_list)
+
+#אמת או שקר בליסט
+my_list=["google","facebook","ebay","apple"]
+print("net4u" in my_list)
