a3f115926a5654990f3dfb371bd3253021428f78
yarin <ptmazkirut@gmail.com>
2021-02-15 15:04:19 +0200

    Conditions

---
+++ tal/targil6.py
@@ -0,0 +1,22 @@
+#Conditions = תנאים
+#if("yarin"=="idan"):
+ #   print("it is good")
+#else:
+ #   print("it isn't good!")
+
+#NAME=input("Enter a name:")
+#if(NAME=="yarin"):
+ #  print("Hello Yarin\n")
+#else:
+ #   print("where is yarin\n ")
+#AGE= input("Enter a age:")
+#if (AGE == "23"):
+ #   print("wow you're 23 years old\n")
+#else:
+ #   print("its not you're real age\n")
+
+number=int(input("you number: \n"))
+if (number==4 or number==5>7):
+    print("good")
+else:
+    print("bad")
