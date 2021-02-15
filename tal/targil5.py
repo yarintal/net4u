9bfb7cd5d3b49845c2131cbf4eebb2aa6f74fcf4
yarin <ptmazkirut@gmail.com>
2021-02-15 13:05:21 +0200

    Lists

---
+++ tal/targil5.py
@@ -0,0 +1,14 @@
+##dictionary:
+
+#my_dict={"www.google.com":"8.8.8.8","www.facebook.com":"7.7.7.7", "www.youtube.com":["5.5.5.5","4.4.4.4"]}
+#print("www.google.com" in my_dict)
+#my_dict2=({"www.net4uc.com":"33.33.33.33","www.groo.co.il":"88.88.88.88"})
+#my_dict.update(my_dict2)
+#print(my_dict)
+#print(my_dict2)
+#print(my_dict)
+#print("Number of my Entries: " +str (len(my_dict)))
+#print(my_dict["www.google.com"])
+#print(my_dict.values())
+#my_dict["www.google.com"]="8.8.4.4"
+#print(my_dict["www.google.com"])
