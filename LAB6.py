59a05051575ab598423237979ef9d8b680d45567
yarin <ptmazkirut@gmail.com>
2021-02-17 13:29:25 +0200

    LAB

--- LAB6.py
+++ LAB6.py
@@ -1,13 +1,28 @@
 from time import sleep
 
-name=input("Enter your name: \n")
-if(name=="yarin"):
-    print("Hello Yarin!")
-elif(name=="Ron"):
-    print("Hello Ron\n")
-elif(name=="igal"):
-    print("Hello Igal")
-else:
-    print("where is my friends?")
+choise=input("Menu:\n1.Insert a number and ** it by 3\n2.here 4 ip as you ask:\n3.here you're  4 DNS:\n4.Check if a string is Polindrom\n")
 
-print("It was a nice game, bye bye\n")
+if(choise=="1"):
+    print("The new number is: " + str((int(input("Enter a number: ")))**3))
+elif(choise=="2"):
+   list_ip=[]
+   list_ip.append(input("Enter new ip: "))
+   list_ip.append(input("Enter new ip: "))
+   list_ip.append(input("Enter new ip: "))
+   list_ip.append(input("Enter new ip: "))
+   print("The new list:\n" + str(list_ip))
+elif(choise=="3"):
+    dns_dict={}
+    dns_dict.update({input("Enter a URL: "): input("Enter a ip ")})
+    dns_dict.update({input("Enter a URL: "): input("Enter a ip ")})
+    dns_dict.update({input("Enter a URL: "): input("Enter a ip ")})
+    dns_dict.update({input("Enter a URL: "): input("Enter a ip ")})
+    print("The new dns list:\n" + str(dns_dict))
+elif(choise=="4"):
+    word=input("Enter a word: ")
+    if (word == word[::-1]):
+        print("This is polindrom!")
+    else:
+        print("This isn't polindrom!")
+else:
+    print("You must pick 1-4 only!")
