b9ae3828ee764c77a34b261889b34489f4ddb8ec
yarin <ptmazkirut@gmail.com>
2021-02-17 12:08:43 +0200

    Conditions

---
+++ LAB6.py
@@ -0,0 +1,13 @@
+from time import sleep
+
+name=input("Enter your name: \n")
+if(name=="yarin"):
+    print("Hello Yarin!")
+elif(name=="Ron"):
+    print("Hello Ron\n")
+elif(name=="igal"):
+    print("Hello Igal")
+else:
+    print("where is my friends?")
+
+print("It was a nice game, bye bye\n")
