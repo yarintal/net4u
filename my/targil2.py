b2f9e1f086ed9cfd3100cf9e956bf9d85bace1ec
yarin <ptmazkirut@gmail.com>
2021-02-18 13:49:48 +0200

    lolaot

---
+++ my/targil2.py
@@ -0,0 +1,37 @@
+#targil 1:
+#print("Net4U, is the best place....\n in the world ")
+
+#import datetime
+#x = datetime.datetime.now()
+#print(x)
+
+#name=input ("Enter you're full name: ")
+#print(name)
+#print((name[::-1]))
+#b=' '.join(name[::-1])
+#print(b)
+
+#filename = input("Input the Filename: ")
+#f_extns = filename.split(".")
+#print ("The extension of the file is : " + repr(f_extns[-1]))
+
+#a = int(input("Input an integer : "))
+#n1 = int( "%s" % a )
+#n2 = int( "%s%s" % (a,a) )
+#n3 = int( "%s%s%s" % (a,a,a) )
+#print (n1+n2+n3)
+
+#num=int(input("Enter a number: "))
+#if(num%2==0):
+ #   print("This number is even\n")
+#else:
+ #   print("this number is odd")
+
+#n = 20
+#d = {"x":200}
+#l = [1,3,5]
+
+#print(type(n)())
+#print(type(d)())
+#print(type(l)())
+
